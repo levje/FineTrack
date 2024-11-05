4d98155dba8acf8e684d4a5c4ecd37af5e48acd3
Jeremi Levesque <jeremilevesque@hotmail.com>
2024-11-05 09:18:15 -0500

    Prediction script to include GM mask for MC exploration

--- ismrm_ttl_predict.sh
+++ ismrm_ttl_predict.sh
@@ -1,15 +1,16 @@
 
-
 # Arguments
 AGENTCHECKPOINT=custom_models/sac_checkpoint/model/last_model_state.ckpt
+# AGENTCHECKPOINT=custom_models/ismrm_1mm_sac_checkpoint/last_model_state.ckpt
 MC_ORACLE_CHECKPOINT=custom_models/ismrm_paper_oracle/ismrm_paper_oracle.ckpt
-OUT_TRACTOGRAM=out_tractograms/ttl_mc_more_rollouts.tck
-NPV=100
+OUT_TRACTOGRAM=out_tractograms/ttl_test.tck
+NPV=10
 
 # Fixed parameters
 IN_ODF=data/datasets/ismrm2015_2mm/fodfs/ismrm2015_fodf.nii.gz
-IN_SEED=data/datasets/ismrm2015_2mm/maps/interface.nii.gz
+IN_SEED=data/datasets/ismrm2015_2mm/masks/ismrm2015_interface.nii.gz
 IN_MASK=data/datasets/ismrm2015_2mm/masks/ismrm2015_wm.nii.gz
+IN_GM_MASK=data/datasets/ismrm2015_2mm/masks/t1_gm_resampled.nii.gz
 python TrackToLearn/runners/ttl_track.py \
     ${IN_ODF} \
     ${IN_SEED} \
@@ -19,5 +20,6 @@ python TrackToLearn/runners/ttl_track.py \
     --max_length 200 \
     --agent_checkpoint ${AGENTCHECKPOINT} \
     --mc_oracle_checkpoint ${MC_ORACLE_CHECKPOINT} \
+    --gm_mask ${IN_GM_MASK} \
     --npv ${NPV} \
     -f
